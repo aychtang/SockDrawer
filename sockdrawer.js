339bfdefa8d06708ce2aa55899c8ed463cdfb92d
Cheeseen <aychtang@gmail.com>
2013-11-03 18:00:52 +0000

    Adds more type defense.

--- sockdrawer.js
+++ sockdrawer.js
@@ -1,6 +1,9 @@
 var eventMap = function(socket, obj) {
-	if (socket) {
+	if (socket && typeof obj === 'object') {
 		for (var ev in obj) {
+			if (!obj[ev]) {
+				obj[ev] = function(){};
+			}
 			if (typeof obj[ev] === 'function') {
 				socket.on(ev, obj[ev]);
 			}
